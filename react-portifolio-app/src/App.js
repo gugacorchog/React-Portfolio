cf4cab3f4a82fbbd5b64783db1c5d94e86573dd8
Gustavo Corchog <gugacorchog@gmail.com>
2022-01-12 18:11:38 +1100

    first commit

---
+++ react-portifolio-app/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import './assets/css/style.css';
+
+const App = () => <PortfolioContainer />;
+
+export default App;
