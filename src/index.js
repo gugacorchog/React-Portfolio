9e84263d718885df9589a77f8c430feece0e19ca
Gustavo Corchog <gugacorchog@gmail.com>
2022-01-24 20:21:16 +1100

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "https://kit.fontawesome.com/5a36f025ca.js";
+// import "https://kit.fontawesome.com/5a36f025ca.js";
 
 
 
